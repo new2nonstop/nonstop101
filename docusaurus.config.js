c3979ed63e71ad0128e91708e7f31315285d2ce8
Shanice Abigail <shanice.abigail@hpe.com>
2024-07-09 11:46:35 +0800

    changed css colour scheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,10 +66,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'HPE NonStop Developer Portal',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'HPE Logo',
+          src: 'img/element.svg',
         },
         items: [
           {
